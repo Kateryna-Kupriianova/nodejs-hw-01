b78166b115a9604170ec41a328d58b25166ff97a
Kateryna-Kupriianova <kupriyanov.forever@gmail.com>
2024-09-27 22:32:14 +0300

    add step 3

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,24 @@
-const generateContacts = async (number) => {};
+
+
+import { faker } from '@faker-js/faker';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+    const contacts = [];
+    for (let i = 0; i < number; i += 1) {
+        contacts.push(createFakeContact());
+    }
+    await writeContacts(contacts);
+
+    function createFakeContact() {
+        return {
+            id: faker.string.uuid(),
+            name: faker.person.fullName(),
+            phone: faker.phone.number(),
+            email: faker.internet.email(),
+            job: faker.person.jobTitle(),
+        };
+    }
+};
 
 generateContacts(5);
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,23 @@
-import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+
+import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'fs/promises';
+
+export const writeContacts = async (newContacts) => {
+    try {
+        
+        const data = await fs.readFile(PATH_DB, 'utf8');
+        const contacts = JSON.parse(data);
+
+        
+        const updatedContacts = [...contacts, ...newContacts];
+
+        
+        await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2), 'utf8');
+    } catch (error) {
+        console.error('Error writing contacts:', error);
+    }
+};
+
+
+

23279fccaebc4938b0ff168d741dd3958d44df36
Kateryna-Kupriianova <kupriyanov.forever@gmail.com>
2024-09-28 22:55:46 +0300

    add step 5

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,14 @@
-export const getAllContacts = async () => {};
+import * as fs from 'fs/promises';
+import { PATH_DB } from '../constants/contacts.js';
+
+export const getAllContacts = async () => {
+    try {
+        const data = await fs.readFile(PATH_DB, 'utf8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.log(error);
+        return [];
+    }
+};
 
 console.log(await getAllContacts());

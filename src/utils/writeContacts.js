c9e6c5e44a0b2a997cccde7068edcc6b710a876b
Kateryna-Kupriianova <kupriyanov.forever@gmail.com>
2024-09-29 09:50:56 +0300

    update writeContacts.js

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,23 +1,10 @@
-
-
 import { PATH_DB } from '../constants/contacts.js';
 import * as fs from 'fs/promises';
 
-export const writeContacts = async (newContacts) => {
+export const writeContacts = async (contacts) => {
     try {
-        
-        const data = await fs.readFile(PATH_DB, 'utf8');
-        const contacts = JSON.parse(data);
-
-        
-        const updatedContacts = [...contacts, ...newContacts];
-
-        
-        await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2), 'utf8');
+      await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2), 'utf8');
     } catch (error) {
         console.error('Error writing contacts:', error);
     }
 };
-
-
-
